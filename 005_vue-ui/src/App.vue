586ec3d9c94904c6f2fc09adf6f5c7e0c0402808
chuxin <15115359055@139.com>
2025-02-08 17:48:54 +0800

    feat: 完善vite功能构建，并且下载自己的@chu-xin/utils 进行测试

--- 005_vue-ui/src/App.vue
+++ 005_vue-ui/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <ComponentA />
-    <ComponentB />
+    <Input />
+    <Button />
   </div>
 </template>
 
 <script setup>
-import ComponentA from './components/ComponentA.vue';
-import ComponentB from './components/ComponentB.vue';
+import {onMounted} from "vue"
+import {add } from "@chu-xin/utils"
+
+import Button from './components/Button/index.vue';
+import Input from './components/Input/index.vue';
+
+
+onMounted(() => {
+  console.log(add(1,2))
+})
 </script>
