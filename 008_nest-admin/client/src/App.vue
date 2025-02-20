fa47fe396e5c88b16c05ba1bb22585bbcb1dfd9e
chuxin <15115359055@139.com>
2025-02-20 13:35:18 +0800

    feat: 删除了中文的配置

--- 008_nest-admin/client/src/App.vue
+++ 008_nest-admin/client/src/App.vue
@@ -6,11 +6,9 @@
 
 <script lang="ts" setup>
 import { useTheme } from '_hooks'
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import { ref } from 'vue'
 const { initTheme } = useTheme()
 initTheme()
 /** 将 Element Plus 的语言设置为中文 */
-const locale = ref(zhCn)
 const button = ref({ autoInsertSpace: true })
 </script>
