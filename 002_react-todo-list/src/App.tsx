20be847757f1005c69b14332fd1a11ef01ccfeff
chuxin <15115359055@139.com>
2025-03-03 10:57:30 +0800

    feat: 基于 useState 实现todo list

--- 002_react-todo-list/src/App.tsx
+++ 002_react-todo-list/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import TodoList from "./pages/useState-TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  )
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <button onClick={() => setCount(count + 1)}>{count}</button>
+            <TodoList/>
+        </>
+    )
 }
 
 export default App
