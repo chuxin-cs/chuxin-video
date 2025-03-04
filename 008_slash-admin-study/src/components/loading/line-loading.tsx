52f635826c849c69e3b6b412f6966c91b0be4cb2
chuxin <15115359055@139.com>
2025-03-04 15:01:20 +0800

    feat: layouts 中集成 loading 组件

---
+++ 008_slash-admin-study/src/components/loading/line-loading.tsx
@@ -0,0 +1,3 @@
+export function LineLoading(){
+    return <>LineLoading...</>
+}
--- 008_slash-admin-study/src/layouts/dashboard/index.tsx
+++ 008_slash-admin-study/src/layouts/dashboard/index.tsx
@@ -1,7 +1,28 @@
 import {Outlet} from "react-router-dom"
+import {Layout} from "antd";
+import {Suspense} from "react";
+
+import Nav from "./nav"
+import Main from "./main"
+import Header from "./header"
+import {CircleLoading} from "@/components/loading";
 
 function DashboardLayout() {
-    return <Outlet></Outlet>
+    const layoutClassName = ""
+
+    return (
+        <Layout className={layoutClassName}>
+            <Suspense fallback={<CircleLoading/>}>
+                <Layout style={{}}>
+                    <Header/>
+                    <Nav/>
+                    <Main/>
+                    <Outlet></Outlet>
+                </Layout>
+
+            </Suspense>
+        </Layout>
+    )
 }
 
 export default DashboardLayout
