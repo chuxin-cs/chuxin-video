63cf3249866a9d90202ce249d94b1da58c22e85d
chuxin <15115359055@139.com>
2025-03-05 13:28:55 +0800

    feat: 集成 styled-components  css in js 方案

--- 008_slash-admin-study/src/pages/home/index.tsx
+++ 008_slash-admin-study/src/pages/home/index.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+
+interface StyleButtonProps {
+    bg?: boolean;
+}
+
+const StyleButton = styled.button.withConfig({
+    shouldForwardProp: (prop) => !['bg'].includes(prop) // 过滤 bg 属性
+})<StyleButtonProps>`
+    color: #fff;
+    border: 0;
+    background: ${(props) => (props.bg ? "#000" : "#666")};
+`
+
 function Home() {
-    return <>home</>
+    return <>
+        <h1>home</h1>
+        <StyleButton bg>111</StyleButton>
+    </>
 }
 
 export default Home
